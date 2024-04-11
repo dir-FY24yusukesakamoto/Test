ab375ccc6bd70ba892e4bc2617fc6853c2ed8068
yusuke.sakamoyo <yusuke.sakamoto.e459@dirbato.co.jp>
2024-04-11 10:57:39 +0900

    Lesson６ 終わり

---
+++ Test/src/calc/CalculatorTest.java
@@ -0,0 +1,25 @@
+package calc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(4, calculator.add(2, 3));
+    }
+
+    @Test
+    void testSubtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(-1, calculator.subtract(2, 3));
+    }
+
+
+}
